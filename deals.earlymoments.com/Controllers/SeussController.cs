7580565a106edf13588e3b935f088cf07fd34c71
Manish Singh <manish.singh@globsyn.com>
2016-01-04 23:06:53 +0530

    add: Seuss Controller

---
+++ deals.earlymoments.com/Controllers/SeussController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace deals.earlymoments.com.Controllers
+{
+    public class SeussController : Controller
+    {
+        //
+        // GET: /Seuss/
+        public ActionResult Four_for_1()
+        {
+            return View();
+        }
+	}
+}
