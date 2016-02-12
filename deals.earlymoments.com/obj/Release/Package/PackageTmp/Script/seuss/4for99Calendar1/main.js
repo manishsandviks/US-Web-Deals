2e250bf8e710429c5e53919ba216f1f0b38ae040
Manish Singh <manish.singh@globsyn.com>
2016-02-12 16:58:50 +0530

    update: application build for deployment

--- deals.earlymoments.com/obj/Release/Package/PackageTmp/Script/seuss/4for99Calendar1/main.js
+++ deals.earlymoments.com/obj/Release/Package/PackageTmp/Script/seuss/4for99Calendar1/main.js
@@ -1,8 +1,41 @@
 
+
+
 $(document).ready(function () {
-    //prettyphoto
-    $("[rel^='lightbox']").prettyPhoto();
-    $("a[rel^='lightbox']").prettyPhoto({ social_tools: false });
+
+    //pretty photo section
+    var totalItems = $('.item').length;
+    var currentIndex = $('div.active').index() + 1;
+    console.log(totalItems + " /" + currentIndex);
+
+    $('.num').html('' + currentIndex + '/' + totalItems + '');
+
+    $('#myCarousel').carousel({
+        interval: 2000
+    });
+
+    $('#myCarousel').bind('slide.bs.carousel', function () {
+        currentIndex = $('div.active').index() + 1;
+        $('.num').html('' + currentIndex + '/' + totalItems + '');
+        console.log(currentIndex);
+    });
+
+
+    $('#playButton').hide();
+    $('#pauseButton').show();
+    //pause play on click
+    $('#playButton').click(function () {
+        $('#myCarousel').carousel('cycle');
+        $('#pauseButton').show();
+        $('#playButton').hide();
+    });
+
+    $('#pauseButton').click(function () {
+        $('#myCarousel').carousel('pause');
+        $('#pauseButton').hide();
+        $('#playButton').show();
+    });
+    //ends pretty photo section
 
 
     //for the cols interchange		
