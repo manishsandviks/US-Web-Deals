978fe04146356af6c251b9b0b47de1a7addb02be
Manish Singh <manish.singh@globsyn.com>
2016-02-23 15:36:38 +0530

    Add: Added new area Max and css and js files

---
+++ deals.earlymoments.com/Areas/Max/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace deals.earlymoments.com.Areas.Max.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Max/Home/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult offers()
+        {
+            return View();
+        }
+
+    }
+}
