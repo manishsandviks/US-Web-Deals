04d46e4be2cbb9c2b80da9215f364a354c01fac0
Srikanth Madishetti <smadishetti@sandviks.com>
2016-03-22 11:55:20 -0400

    update: capturing query string params

--- deals.earlymoments.com/Services/OfferService.cs
+++ deals.earlymoments.com/Services/OfferService.cs
@@ -10,31 +10,18 @@ namespace deals.earlymoments.com.Services
     {
         public ShippingModels.ShippingAddress GetDefaultCustomerInfo()
         {
-            //@sFirstName=#fname
-            //&sLastName=#lname
-            //&sAddress1=#street
-            //&sAddress2=
-            //&sCity=#city
-            //&sState=#state
-            //&sZipCode=#zip_5
-            //&email=#email
-            //&confEmail=#email
-            //&phone1=
-            //&phone2=
-            //&phone3= 
-
-            ShippingModels.ShippingAddress shippingAddress = new ShippingModels.ShippingAddress
+            var shippingAddress = new ShippingModels.ShippingAddress
             {
-                ShippingFirstName = HttpContext.Current.Request.QueryString["sFirstName"] as string
-                , ShippingLastName = HttpContext.Current.Request.QueryString["sLastName"] as string
-                , ShippingAddress1 = HttpContext.Current.Request.QueryString["sAddress1"] as string
-                , ShippingAddress2 = HttpContext.Current.Request.QueryString["sAddress2"] as string
-                , ShippingCity = HttpContext.Current.Request.QueryString["sCity"] as string
-                , ShippingState = HttpContext.Current.Request.QueryString["sState"] as string
-                , ShippingZipCode = HttpContext.Current.Request.QueryString["sZipCode"] as string
-                , ShippingEmail = HttpContext.Current.Request.QueryString["email"] as string
-                , ShippingConfirmEmail = HttpContext.Current.Request.QueryString["confEmail"] as string
-                , ShippingPhone = HttpContext.Current.Request.QueryString["phone1"] as string
+                ShippingFirstName = System.Web.HttpContext.Current.Request.QueryString["sFirstName"] as string
+                ,ShippingLastName = System.Web.HttpContext.Current.Request.QueryString["sLastName"] as string
+                ,ShippingAddress1 = System.Web.HttpContext.Current.Request.QueryString["sAddress1"] as string
+                ,ShippingAddress2 = System.Web.HttpContext.Current.Request.QueryString["sAddress2"] as string
+                ,ShippingCity = System.Web.HttpContext.Current.Request.QueryString["sCity"] as string
+                ,ShippingState = System.Web.HttpContext.Current.Request.QueryString["sState"] as string
+                ,ShippingZipCode = System.Web.HttpContext.Current.Request.QueryString["sZipCode"] as string
+                ,ShippingEmail = System.Web.HttpContext.Current.Request.QueryString["email"] as string
+                ,ShippingConfirmEmail = System.Web.HttpContext.Current.Request.QueryString["confEmail"] as string
+                ,ShippingPhone = System.Web.HttpContext.Current.Request.QueryString["phone1"] as string
                 //phone2
                 //phone3
             };
