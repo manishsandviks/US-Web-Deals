224172cdf042b6af2c0345082a1cf2dfd4631ada
Srikanth Madishetti <smadishetti@sandviks.com>
2016-04-01 11:01:05 -0400

    Updates: routing file to made seuss offer as default at domain root level

--- deals.earlymoments.com/App_Start/RouteConfig.cs
+++ deals.earlymoments.com/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace deals.earlymoments.com
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Seuss", action = "four_for_1_spring", id = UrlParameter.Optional }
             );
         }
     }
