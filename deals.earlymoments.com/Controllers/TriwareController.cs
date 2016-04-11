11766440a0af2676e0f01d33e6ad924780fb933d
Manish Singh <manish.singh@globsyn.com>
2016-04-11 15:33:47 +0530

    Added: Triware Controller and all blank pages added

---
+++ deals.earlymoments.com/Controllers/TriwareController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace deals.earlymoments.com.Controllers
+{
+    public class TriwareController : Controller
+    {
+        //
+        // GET: /Triwire/
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult home()
+        {
+            return View();
+        }
+        public ActionResult shipping()
+        {
+            return View();
+        }
+        public ActionResult upsell_offer1()
+        {
+            return View();
+        }
+        public ActionResult upsell_offer2()
+        {
+            return View();
+        }
+        public ActionResult thankyou()
+        {
+            return View();
+        }
+    }
+}
